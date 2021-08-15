d92b14360d4124960aae3fd602356c005955df59
Minh <vuongthanhminh2011@gmail.com>
2021-08-15 09:17:03 +0700

    commit

--- include/connect.php
+++ include/connect.php
@@ -2,7 +2,7 @@
 $server = "13.212.100.240";
 $user = "your_username";
 $pass = "your-password";
-$database = "php";
+$database = "minhpro"; // thế để ntn // thế phải để nhu thế chỗ php à// để cái tên db tạo hôm qua ấy//thế chỗ database để minhpro là cái tên t để trong cái s
 $connect = mysqli_connect($server,$user,$pass,$database); 
 if (!$connect) {
 	die("Connect Failed:".mysqli_connect_error());
